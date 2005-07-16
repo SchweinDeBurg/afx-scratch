0be4403dfe667d5037bdb2ab04b61311fd2e46c7
Elijah Zarezky <Elijah Zarezky@9edebcd1-9b41-0410-8d36-53a5787adf6b>
2005-07-16 18:20:08 +0000

    Improved project templates.

--- trunk/AppData/Sources/AfxConsole/Source/stdafx.h
+++ trunk/AppData/Sources/AfxConsole/Source/stdafx.h
@@ -25,8 +25,8 @@
 #include <afxwin.h>			// MFC core and standard components
 
 // force ISO/IEC 14882 conformance in for loop scope
-#if _MSC_VER < 1300
-#define for if(false); else for
+#if (_MSC_VER < 1300)
+#define for if (false); else for
 #else
 #pragma conform(forScope, on)
 #endif	// _MSC_VER
