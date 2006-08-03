88d8235f49f2baea2082ec123bac8654d157df74
Elijah <Elijah@9edebcd1-9b41-0410-8d36-53a5787adf6b>
2006-08-03 09:18:26 +0000

    Updated copyright notice in the source files.

--- trunk/Source/CustomGroupBox.h
+++ trunk/Source/CustomGroupBox.h
@@ -1,5 +1,5 @@
 // AfxScratch application.
-// Copyright (c) 2004-2005 by Elijah Zarezky,
+// Copyright (c) 2004-2006 by Elijah Zarezky,
 // All rights reserved.
 
 // CustomGroupBox.h - interface of the CCustomGroupBox class
