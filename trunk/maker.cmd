3072c1b2aae32581626d311cd275378583d4552e
Elijah <Elijah@9edebcd1-9b41-0410-8d36-53a5787adf6b>
2006-08-16 09:01:30 +0000

    Slightly improved build script.

--- trunk/maker.cmd
+++ trunk/maker.cmd
@@ -5,13 +5,15 @@ echo.
 devenv.com AfxScratch70.sln /rebuild MBCS_Release /project AfxScratch /nologo
 echo Compiling documentation...
 echo.
-cd .\HTML
+chdir .\HTML
 if exist AfxScratch.chm del AfxScratch.chm
 hhc.exe AfxScratch.hhp
+chdir ..\
 echo.
 echo.
 echo Creating installer...
 echo.
+if not exist .\Setup mkdir .\Setup
 cd ..\Source
 if exist ..\Setup\*.exe del ..\Setup\*.exe
 iscc.exe Setup.iss
