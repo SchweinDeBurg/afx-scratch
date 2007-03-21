39825eeb3dbab35069c30e05ee62cdc5b3a6641b
elijah <elijah@9edebcd1-9b41-0410-8d36-53a5787adf6b>
2007-03-21 16:12:05 +0000

    Tagged release 1.0.2222.

--- makeall.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo Building executable - "Win32 MBCS Debug"...
-echo.
-devenv.com AfxScratch71.sln /rebuild MBCS_Debug /project AfxScratch /nologo
-echo Building executable - "Win32 MBCS Release"...
-echo.
-devenv.com AfxScratch71.sln /rebuild MBCS_Release /project AfxScratch /nologo
-echo.
