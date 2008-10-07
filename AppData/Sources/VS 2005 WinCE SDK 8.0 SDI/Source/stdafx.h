81fe16f8f41222b8c752d79cd89fcd6c10095d07
Elijah Zarezky <Elijah Zarezky@9edebcd1-9b41-0410-8d36-53a5787adf6b>
2008-10-07 04:57:12 +0000

    Improved project templates. Removed unneeded references to the inherited property sheet.

--- AppData/Sources/VS 2005 WinCE SDK 8.0 SDI/Source/stdafx.h
+++ AppData/Sources/VS 2005 WinCE SDK 8.0 SDI/Source/stdafx.h
@@ -12,7 +12,10 @@
 
 #if defined(_MSC_VER) && (_MSC_VER > 1000)
 #pragma once
-#endif	// _MSC_VER
+#endif   // _MSC_VER
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// unwanted warnings
 
 // unreferenced inline/local function has been removed
 #pragma warning(disable: 4514)
@@ -21,34 +24,43 @@
 // identifier was truncated in the debug information
 #pragma warning(disable: 4786)
 
-#define WINVER _WIN32_WCE
-
 #pragma warning(push)
 
 // nonstandard extension used: nameless struct/union
 #pragma warning(disable: 4201)
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 // PSDK headers
+
+#define WINVER _WIN32_WCE
+
 #include <windows.h>
 #include <aygshell.h>
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 // CRT headers
+
 #include <altcecrt.h>
 #include <tchar.h>
 #include <typeinfo>
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 // DRA headers
+
 #include <DeviceResolutionAware.h>
 
 #pragma warning(pop)
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// some tricks
 
 // force ISO/IEC 14882 conformance in for loop scope
 #if (_MSC_VER < 1300)
 #define for if (false); else for
 #else
 #pragma conform(forScope, on)
-#endif	// _MSC_VER
+#endif   // _MSC_VER
 
-#endif	// __stdafx_h
+#endif   // __stdafx_h
 
 // end of file
